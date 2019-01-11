566be5583f632e23699e6d98721b02e2cf8a1125
143608 <143608@S020RMD114-12>
2019-01-11 10:14:25 -0600

    Added explosion images and enemy movement

--- Galaga/Galaga/Galaga/Game1.cs
+++ Galaga/Galaga/Galaga/Game1.cs
@@ -26,17 +26,30 @@ namespace Galaga
         List<Rectangle> playerShots;
         int fireTime;
 
-
+        //sprite location on the screen
         Rectangle ship;
         Rectangle spaceFly;
         Rectangle playBullet; //player's bullet
         Rectangle enBullet; //enemy bullet
+        Rectangle explosion;
 
         //sprite location on picture
         Rectangle spaceFly1;
         Rectangle pBull1;
         Rectangle eBull1;
-        //363 193
+        Rectangle explosion1;
+        Rectangle explosion2;
+        Rectangle explosion3;
+        Rectangle explosion4;
+        Rectangle explosion5;
+
+        //list of enemies
+        Rectangle[] enemies;
+        int move;
+
+        //lives and score
+        int life;
+        int score;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -52,7 +65,6 @@ namespace Galaga
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            ship = new Rectangle(10, 10, 35, 35);
             old = Keyboard.GetState();
 
             playerShots = new List<Rectangle>();
@@ -64,6 +76,7 @@ namespace Galaga
 
             playBullet = new Rectangle();
             enBullet = new Rectangle();
+            explosion = new Rectangle();
 
 
             //on sprite sheet
@@ -71,6 +84,22 @@ namespace Galaga
 
             pBull1 = new Rectangle(364, 193, 20, 20);
             eBull1 = new Rectangle(372, 49, 20, 20);
+
+            explosion1 = new Rectangle(208, 187, 20, 20);
+            explosion2 = new Rectangle(229, 187, 20, 20);
+            explosion3 = new Rectangle(251, 187, 20, 20);
+            explosion4 = new Rectangle(278, 187, 20, 20);
+            explosion5 = new Rectangle(314, 187, 20, 20);
+
+
+            //enemy list
+            enemies = new Rectangle[] { spaceFly };
+            move = 3;
+
+
+            //life and score
+            life = 3;
+            score = 0;
             base.Initialize();
         }
 
@@ -101,6 +130,23 @@ namespace Galaga
         /// checking for collisions, gathering input, and playing audio.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        /// 
+        public void enemyMovement()
+        {
+            //for (int i = 0; i < enemies.Length; i++)
+            //{
+            //    enemies[i].X += move;
+            //    if (enemies[i].X + enemies[i].Width > GraphicsDevice.Viewport.Width || enemies[i].X < 10)
+            //    {
+            //        move *= -1;
+            //    }
+            //}
+            spaceFly.X += move;
+            if (spaceFly.X + spaceFly.Width > GraphicsDevice.Viewport.Width || spaceFly.X < 0)
+            {
+                move *= -1;
+            }
+        }
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
@@ -119,6 +165,13 @@ namespace Galaga
                 fireTime++;
             }
 
+            enemyMovement();
+
+            //extra life based on scoring
+            if (score == 30000 || score == 80000)
+            {
+                life++;
+            }
             base.Update(gameTime);
         }
 
@@ -136,8 +189,6 @@ namespace Galaga
             spriteBatch.Draw(galagaSpriteSheet, playBullet, pBull1, Color.White);
             spriteBatch.Draw(galagaSpriteSheet, enBullet, eBull1, Color.White);
             spriteBatch.Draw(galagaSpriteSheet, spaceFly, spaceFly1, Color.White);
-            for (int i = 0; i < playerShots.Count; i++)
-                spriteBatch.Draw(galagaSpriteSheet, playerShots[i], new Rectangle(0, 0, 10, 10), Color.White);
             spriteBatch.End();
             base.Draw(gameTime);
         }
